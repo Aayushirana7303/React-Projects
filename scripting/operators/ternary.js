283c229a3b3e0729f5896703923946c42ec71bb8
Aayushi <aayushisingh7303@gmail.com>
2026-04-08 19:34:34 +0530

    update message

--- scripting/operators/ternary.js
+++
@@ -1,4 +0,0 @@
-// SYNTAX --->>   (condition) ? (statement1): (statement2);
-
-var myage = 20;
-(myage >= 18) ? (console.log("You can Drive")): (console.log("Cannot Drive"));
--- scripting/string.js
+++
@@ -1,7 +0,0 @@
-const name = "Aayushi"
-const repoCount = 50
-
-// console.log(name + repoCount + " Value"); not good for developers use in now days
-
-// String Interpolation method
-console.log(`Hello my name is ${name} and my repoCount is ${repoCount}`);
